abc2f44dee8d3b36969dcb3ffb16c97d185a1084
Ivan Voevodov <i.voevodov@trackad.ai>
2024-04-02 14:55:00 +0300

    Added funnel graph to image entry

--- src/components/image.js
+++ src/components/image.js
@@ -1,7 +1,9 @@
+import FunnelGraph from './image/FunnelGraph.vue';
 import LineChart from './image/LineChart.vue';
 import LoaderLineScale from './image/LoaderLineScale.vue';
 
 export {
+    FunnelGraph,
     LineChart,
     LoaderLineScale,
 }
