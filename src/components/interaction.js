02e11901da343f22165534b0a026569ccafb4336
Ivan Voevodov <i.voevodov@trackad.ai>
2024-04-19 07:01:18 +0000

    Added tree multiselect component

--- src/components/interaction.js
+++ src/components/interaction.js
@@ -1,5 +1,9 @@
 import Dropdown from './interaction/Dropdown.vue';
+import Input from './interaction/Input.vue';
+import TreeMultiselect from './interaction/TreeMultiselect.vue';
 
 export {
     Dropdown,
+    Input,
+    TreeMultiselect,
 }
