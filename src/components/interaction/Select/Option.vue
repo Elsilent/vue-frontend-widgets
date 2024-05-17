a37374169fb246947b6d1c306fbfd44e05d63bbe
Anna Pertseva <a.pertseva@trackad.ai>
2024-05-17 10:42:43 +0400

    TRACKAD-7505 build improvement, element-plus integration

---
+++ src/components/interaction/Select/Option.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ElOption } from 'element-plus';
+import 'element-plus/es/components/option/style/index';
+defineProps(['value']);
+</script>
+
+<template lang="pug">
+ElOption(:value="value")
+  slot
+</template>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+@import '../../../styles/colors.scss';
+
+.el-select-dropdown__item {
+  margin: 0 2px 0 -1px;
+  &.is-selected {
+    background-color: map.get(map.get($themes, 'light'), 'background-list-accent');
+    font-weight: normal;
+    color: map.get(map.get($themes, 'light'), 'white');
+  }
+}
+</style>
