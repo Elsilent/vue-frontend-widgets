e02dc4df1faf928586fe61f6e009b31d0ad8ea38
Anna P <roomy@list.ru>
2024-08-02 11:00:29 +0400

    fixes for common table

--- src/components/view/DetailsSelector.vue
+++ src/components/view/DetailsSelector.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { onUnmounted, ref, toRefs } from 'vue';
+import { ref, toRefs } from 'vue';
 import Align from '../container/Align.vue';
 import Icon from '../image/Icon.vue';
 import Info from '../label/Info.vue';
-import BodyPopover from '../container/BodyPopover.vue';
+import { ElPopover, ClickOutside as vClickOutside } from 'element-plus';
 
 const props = withDefaults(
   defineProps<{
@@ -19,44 +19,20 @@ const props = withDefaults(
 const { labels, open, title } = toRefs(props);
 
 const selectorVisible = ref(false);
-const popover = ref<typeof BodyPopover | undefined>();
-const root = ref<HTMLElement | undefined>();
+const popover = ref<typeof ElPopover | undefined>();
 
 const emit = defineEmits<{
   (e: 'hideDetails'): void;
   (e: 'showDetails', kind: string): void;
 }>();
 
-const onSelectorBlur = (event: MouseEvent) => {
-  if (
-    !event.target ||
-    root.value?.contains(event.target as HTMLElement) ||
-    popover.value?.$el.contains(event.target as HTMLElement)
-  ) {
-    return;
-  }
-
-  selectorVisible.value = false;
-
-  window.removeEventListener('mouseup', onSelectorBlur);
-};
-
 const onSelectorFocus = () => {
   if (open.value) {
     emit('hideDetails');
-
     return;
   }
 
-  if (selectorVisible.value) {
-    selectorVisible.value = false;
-
-    return;
-  }
-
-  selectorVisible.value = true;
-
-  window.addEventListener('mouseup', onSelectorBlur);
+  selectorVisible.value = !selectorVisible.value;
 };
 
 const toggleDetails = (kind: string) => {
@@ -64,75 +40,74 @@ const toggleDetails = (kind: string) => {
 
   if (open.value) {
     emit('hideDetails');
-
     return;
   }
-
   emit('showDetails', kind);
 };
 
-onUnmounted(() => {
-  window.removeEventListener('mouseup', onSelectorBlur);
-});
+const onClickOutside = () => {
+  selectorVisible.value = false;
+};
 </script>
 
 <template lang="pug">
 .details-selector-container(
-  ref='root',
-  @click.stop='() => onSelectorFocus()',
+  @click='onSelectorFocus'
 )
-  Align.details-selector(vertical='center')
-    Info(
-      v-if="title",
-      mood='important-alt',
-      size='small',
-    ) {{ title }}
-    Icon(
-      :value="open ? 'chevron-up' : 'chevron-down'",
-      mood='important-alt',
-      size="small-2",
-    )
-  BodyPopover(
-    ref='popover',
-    :parentNode='root',
-    :visible="selectorVisible",
-    autoPosition,
-    popoverClass="details-selector-popover",
+  ElPopover(
+    :show-arrow="false"
+    popper-class="details-selector-popover"
+    :teleported="false"
+    :persistent="false"
+    :visible="selectorVisible"
+    :hide-after="0"
+    :offset="0"
+    ref="popover"
   )
+    template(#reference)
+      Align.details-selector(vertical='center' v-click-outside="onClickOutside")
+        Info(
+          v-if="title",
+          mood='important-alt',
+          size='small',
+        ) {{ title }}
+        Icon(
+          :value="open ? 'chevron-up' : 'chevron-down'",
+          mood='important-alt',
+          size="small-2",
+        )
     Info.dropdown-item(
       v-for='(label, kind) in labels',
       @click.stop="() => toggleDetails(kind)",
       contrast,
     ) {{ label }}
+
 </template>
 
 <style lang="scss">
-.popover.details-selector-popover {
-  min-width: 10rem;
-  padding: 0.5rem 0;
-
-  > .dropdown-item {
-    cursor: pointer;
-    padding: 0.25rem 1.5rem;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
 @import '../../styles/colors.scss';
-
-.dropdown-item {
-  transition: background-color 0.3s;
-  &:active {
-    @include apply-color(background-color, background-accent);
-  }
-
-  &:not(:active):hover {
-    @include apply-color(background-color, background-lowered);
-  }
-}
+@import '../../styles/spacing.scss';
 
 .details-selector-container {
+  .details-selector-popover {
+    --el-popover-padding: 0.5rem 0;
+    min-width: 10rem;
+  }
+
+  .dropdown-item {
+    transition: background-color 0.3s;
+    cursor: pointer;
+    padding: 0.25rem 1.5rem;
+    text-align: left;
+    &:active {
+      @include apply-color(background-color, background-accent);
+    }
+
+    &:not(:active):hover {
+      @include apply-color(background-color, background-lowered);
+    }
+  }
+
   > .details-selector {
     @include apply-color(color, text-important-alt);
 
