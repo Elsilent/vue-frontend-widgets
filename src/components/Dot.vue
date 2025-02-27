d1428a7e0ba556cde3fbd7822dc3da03efb52280
AnnShabanova <roomy@list.ru>
2025-02-27 12:55:46 +0300

    fixed dot size

--- src/components/Dot.vue
+++ src/components/Dot.vue
@@ -45,6 +45,7 @@ $-chart-colors: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
   margin-right: $padding-size-small;
   height: $padding-size-small;
   width: $padding-size-small;
+  min-width: $padding-size-small;
 
   @each $chart in $-chart-colors {
     &.chart-#{$chart} {
